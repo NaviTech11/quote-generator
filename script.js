be5997efefe51d894b610403db75dbaba82aad85
Ivan Hernandez <ivan.hdz2@outlook.com>
2020-08-28 10:42:16 -0500

    Create Loader Spinner and Code Refactor

--- script.js
+++ script.js
@@ -3,31 +3,48 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
+
+
+function showLoadingSpinner() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+
+function hideLoadingSpinner(){
+    if (!loader.hidden){
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+}
 
 // Get Quote API
 async function getQuote() {
+    showLoadingSpinner();
+    //We need to use a Proxy URL to male our API call in order to avoid CORS error
     const proxyUrl = "https://limitless-crag-75725.herokuapp.com/";
     const apiUrl = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
     try {
         const response = await fetch(proxyUrl + apiUrl);
         const data = await response.json();
        
-        //If author is blank, add "Unknown"
+        //Check if author is blank, add "Unknown"
         if (data.quoteAuthor === "") {
             authorText.innerText = "Unkown";
         } else {
             authorText.innerText = data.quoteAuthor;
         }
 
-        //Reduce font size for long quotes
+        //Dynamically reduce font size for long quotes
         if (data.quoteText.length > 120) {
             quoteText.classList.add("long-quote");
         } else {
             quoteText.classList.remove("long-quote");
         }
-
         quoteText.innerText = data.quoteText;
-
+        //Stop Loader, Show Quote
+        hideLoadingSpinner();
     } catch (error) {
         getQuote();
        
